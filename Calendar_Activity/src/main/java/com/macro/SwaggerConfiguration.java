675d8ac47330c3c7f99e518f46bd852ab13d9625
Juan Pablo <jpjimenez94@hotmail.com>
2019-09-18 08:53:51 -0500

    Configuracion Swagger

--- Calendar_Activity/src/main/java/com/macro/SwaggerConfiguration.java
+++ Calendar_Activity/src/main/java/com/macro/SwaggerConfiguration.java
@@ -1,28 +1,35 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.macro;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-import static springfox.documentation.builders.PathSelectors.regex;
+
 @Configuration
 @EnableSwagger2
 public class SwaggerConfiguration {
-  
-   @Bean
-      public Docket productApi() {
-          return new Docket(DocumentationType.SWAGGER_2)
-        .select()
-        .apis(RequestHandlerSelectors.basePackage("com.arquitecturajava.rest"))
-               .paths(regex("/getCalendar.*"))
-               .build();
-      }
-            
+
+    @Bean
+    public Docket customDocket() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(getApiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.macro.api"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo getApiInfo() {
+        return new ApiInfoBuilder()
+                .title("MicroService API Documentation")
+                .description("MicroService API Description")
+                .version("1.0")
+                .build();
+    }
+
 }
